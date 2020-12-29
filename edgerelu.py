1df53fd1a32c517bccaa4db1f0ff51f8a66ecda5
yfzhang <yifeiacc@gmail.com>
2020-12-29 19:14:07 +0800

    0.1.7:ReLU, PReLU, ELU, LReLU

--- edgerelu.py
+++ edgerelu.py
@@ -157,6 +157,96 @@ class EdgeReluV1(MessagePassing):
         return result.t()
 
 
+# class EdgeReluV2(MessagePassing):
+#     def __init__(self, channels,
+#                  k=2, reduction=2,
+#                  add_self_loops=True,
+#                  negative_slope=0.2,
+#                  **kwargs):
+#         kwargs.setdefault('aggr', 'mean')
+#         super(EdgeReluV2, self).__init__(node_dim=0, **kwargs)
+#         self.negative_slope = negative_slope
+#         self.channels = channels
+#         self.k = k
+#         self.add_self_loops = add_self_loops
+#         self.fc1 = GCNConv(channels,  channels // reduction)
+
+#         self.relu = nn.ReLU(inplace=True)
+#         self.fc2 = nn.Linear(channels // reduction, 2*k*channels)
+#         self.sigmoid = nn.Sigmoid()
+
+#         self.register_buffer('lambdas', torch.Tensor([1.]*k + [0.5]*k).float())
+#         self.register_buffer('init_v', torch.Tensor(
+#             [1.] + [0.]*(2*k - 1)).float())
+
+#         self.att_l = Parameter(torch.Tensor(1, channels))
+#         self.att_r = Parameter(torch.Tensor(1, channels))
+#         self.reset_parameters()
+
+#     def get_relu_coefs(self, x, edge_index):
+#         theta = x
+#         theta = self.fc1(theta, edge_index)
+#         theta = theta.mean(dim=0)
+#         theta = self.relu(theta)
+#         theta = self.fc2(theta)
+#         # theta = 2 * self.sigmoid(theta) - 1
+#         theta = F.tanh(theta)
+#         return theta
+
+#     def reset_parameters(self):
+#         glorot(self.att_l)
+#         glorot(self.att_r)
+
+#     def forward(self, x, edge_index, size=None):
+#         x_l = x_r = x
+#         alpha_l = (x_l * self.att_l).sum(dim=-1)
+#         alpha_r = (x_r * self.att_r).sum(dim=-1)
+#         # print(alpha_l.shape)
+#         # print(alpha_r.shape)
+#         if self.add_self_loops:
+#             if isinstance(edge_index, Tensor):
+#                 num_nodes = x_l.size(0)
+#                 if x_r is not None:
+#                     num_nodes = min(num_nodes, x_r.size(0))
+#                 if size is not None:
+#                     num_nodes = min(size[0], size[1])
+#                 edge_index, _ = remove_self_loops(edge_index)
+#                 edge_index, _ = add_self_loops(edge_index, num_nodes=num_nodes)
+#             elif isinstance(edge_index, SparseTensor):
+#                 edge_index = set_diag(edge_index)
+
+#         _, col = edge_index[0], edge_index[1]
+#         self.degree = degree(col)
+
+#         theta = self.get_relu_coefs(x, edge_index)
+#         self.theta = theta.view(-1, self.channels, 2 * self.k)
+#         out = self.propagate(edge_index, x=(x_l, x_r),
+#                              alpha=(alpha_l, alpha_r), size=size)
+#         return out
+
+#     def message(self,  x_j, alpha_j, alpha_i, index, ptr, size_i):
+#         alpha = alpha_j if alpha_i is None else alpha_j + alpha_i
+#         alpha = F.leaky_relu(alpha, self.negative_slope)
+
+#         gamma = self.degree[index]/3
+#         alpha = alpha / 10
+#         alpha = softmax(alpha, index, ptr, size_i) * gamma
+
+#         alpha = torch.min(alpha, torch.ones_like(alpha))
+
+#         alpha = alpha.view(-1, 1, 1)
+#         # relu_coefs = (alpha * self.theta) * self.lambdas + self.init_v
+#         relu_coefs = (self.theta * self.lambdas + self.init_v) * alpha
+#         # relu_coefs = F.dropout(relu_coefs, 0.2, training=self.training)
+
+#         x = x_j
+#         x = x.unsqueeze(-1)
+#         x_perm = x.permute(2, 0, 1).unsqueeze(-1)
+#         output = x_perm * relu_coefs[:, :, :self.k] + relu_coefs[:, :, self.k:]
+#         result = torch.max(output, dim=-1)[0].permute(1, 2, 0).squeeze()
+#         return result
+
+
 class EdgeReluV2(MessagePassing):
     def __init__(self, channels,
                  k=2, reduction=2,
@@ -170,13 +260,13 @@ class EdgeReluV2(MessagePassing):
         self.k = k
         self.add_self_loops = add_self_loops
         self.fc1 = GCNConv(channels,  channels // reduction)
-
         self.relu = nn.ReLU(inplace=True)
         self.fc2 = nn.Linear(channels // reduction, 2*k*channels)
         self.sigmoid = nn.Sigmoid()
 
         self.register_buffer('lambdas', torch.Tensor([1.]*k + [0.5]*k).float())
-        self.register_buffer('init_v', torch.Tensor([1.] + [0.]*(2*k - 1)).float())
+        self.register_buffer('init_v', torch.Tensor(
+            [1.] + [0.]*(2*k - 1)).float())
 
         self.att_l = Parameter(torch.Tensor(1, channels))
         self.att_r = Parameter(torch.Tensor(1, channels))
@@ -214,8 +304,11 @@ class EdgeReluV2(MessagePassing):
             elif isinstance(edge_index, SparseTensor):
                 edge_index = set_diag(edge_index)
 
-        _, col = edge_index[0], edge_index[1]
-        self.degree = degree(col)
+        if isinstance(edge_index, SparseTensor):
+            self.degree = edge_index.sum(dim=0)
+        else:
+            _, col = edge_index[0], edge_index[1]
+            self.degree = degree(col)
 
         theta = self.get_relu_coefs(x, edge_index)
         self.theta = theta.view(-1, self.channels, 2 * self.k)
@@ -226,21 +319,21 @@ class EdgeReluV2(MessagePassing):
     def message(self,  x_j, alpha_j, alpha_i, index, ptr, size_i):
         alpha = alpha_j if alpha_i is None else alpha_j + alpha_i
         alpha = F.leaky_relu(alpha, self.negative_slope)
-
         gamma = self.degree[index]/3
+        # self.degree = None
         alpha = alpha / 10
         alpha = softmax(alpha, index, ptr, size_i) * gamma
-
         alpha = torch.min(alpha, torch.ones_like(alpha))
-
         alpha = alpha.view(-1, 1, 1)
-        # relu_coefs = (alpha * self.theta) * self.lambdas + self.init_v
-        relu_coefs = (self.theta * self.lambdas + self.init_v) * alpha
-        relu_coefs = F.dropout(relu_coefs, 0.2, training=self.training)
-        
-        x = x_j
+        relu_coefs = self.theta * self.lambdas * alpha + self.init_v
+        # relu_coefs = F.dropout(relu_coefs, 0.2, training=self.training)
+        return self.active(x_j, relu_coefs, self.k)
+
+    def active(self, x, coef, k):
+        # x is (#node/#edge, #channel)
+        # coef (#node/#edge, #channel, #parameter)
         x = x.unsqueeze(-1)
         x_perm = x.permute(2, 0, 1).unsqueeze(-1)
-        output = x_perm * relu_coefs[:, :, :self.k] + relu_coefs[:, :, self.k:]
+        output = x_perm * coef[:, :, :k] + coef[:, :, k:]
         result = torch.max(output, dim=-1)[0].permute(1, 2, 0).squeeze()
         return result
